fdb5fc020aab72fd58f62cf612b27c1cd5a19202
Tara <adela9308@yahoo.com>
2023-12-18 23:24:51 +0200

    Teacher website

---
+++ src/main/java/com/example/teacherwebsite/TeacherWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.teacherwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teacherwebsite/TeacherWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
